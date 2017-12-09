9fc56892be55a70999a8b25f5775ba244a3108dc
Umar Safdar <umarsafdar@Umars-MacBook-Air.local>
2017-12-09 13:29:36 +0500

    Initial commit

---
+++ app/src/main/java/pk/edu/uoslahore/personalmedicalassistant/adapter/MedicineAdapter.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright (c) 2017. Umar Safdar All Rights Reserved.
+ */
+
+package pk.edu.uoslahore.personalmedicalassistant.adapter;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.ViewGroup;
+
+/**
+ * Created by umarsafdar on 09/12/2017.
+ */
+
+public class MedicineAdapter extends RecyclerView.Adapter<MedicineAdapter.MedicineHolder> {
+    @Override
+    public MedicineHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(MedicineHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class MedicineHolder{
+
+
+    }
+}
